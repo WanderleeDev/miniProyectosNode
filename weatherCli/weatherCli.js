c5c82f8256f934ec291edf0011e6ef369f9b5777
Wanderlee Max <wanderleedev@gmail.com>
2023-08-07 00:16:25 -0500

    app del clima terminada

--- weatherCli/weatherCli.js
+++ weatherCli/weatherCli.js
@@ -3,15 +3,39 @@ import axios from 'axios'
 
 const API_KEY = "bf3169621df25d8a0a19e99f395dcb83";
 
+//  formatea la data para presentarla en consola
+function formatData(data) {
+  const table = [
+    {Data: 'Description', Result: data.weather[0].description },
+    {Data: 'Temp', Result: `${data?.main?.temp ?? 'NO DATA'} °C`},
+    {Data: 'Humidity', Result: `${data?.main?.humidity ?? 'NO DATA'} %`},
+    {Data: 'Speed Wind', Result: `${data?.wind?.speed ?? 'NO DATA'} %`}
+  ]
+  console.log(chalk.blueBright(data.name));
+  console.table(table);
+}
+
+
+//  trae la data usando axios
 async function consultarClima(city) {
   try {
-    
+    let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    const res = await axios.get(endpoint, {
+      params: {
+        q: city,
+        appid: API_KEY,
+        units: "metric"
+      }
+    })
+    return res.data
+
   } catch (error) {
     console.log(chalk.red(`Nose pudo obtener el clima de: ${city}`));
     throw new Error(error)
   }
 }
 
+//  función inicial que recibe el país para traer la data y formatearlo
 function traerDatos()  {
   let ciudad = process.argv[2]
 
@@ -21,11 +45,17 @@ function traerDatos()  {
       chalk.red("Formato válido: pnpm run clima [ciudad] o node weatherCli/weatherCli.js [ciudad]")
     );
   }
-  console.log(ciudad);
+
+  //Trae la data del país o ciudad
+  consultarClima(ciudad)
+    .then(data => formatData(data))
+    .catch(err => {
+      console.log(`Error: ${err}`)
+      process.exit(1)
+    })
 }
-
+//Iniciar el proceso
 traerDatos()
-
 
 /*process.argv =>  retorna un array con los siguientes datos: 
 **[
